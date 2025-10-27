c2814f6cf6dda36176301110535fdddab45c2510
raulgar19 <raulgamu2003@gmail.com>
2025-10-27 14:15:23 +0100

    first commit

---
+++ src/components/HijoDeportes.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 style="color: red">{{ nombredeporte }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HijoDeporte",
+  props: ["nombredeporte"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 style="color: blue">Padre deportes</h1>
+    <ul style="list-style-type: none">
+      <li v-for="sport in deportes" :key="sport">
+        <HijoDeportes :nombredeporte="sport" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HijoDeportes from "./HijoDeportes.vue";
+
+export default {
+  name: "PadreDeportes",
+  components: {
+    HijoDeportes,
+  },
+  data() {
+    return {
+      deportes: ["Curling", "Peonza", "Canicas", "Bolos"],
+    };
+  },
+};
+</script>
+
+<style></style>
